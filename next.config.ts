73be3ef521be6951aa075befab126f40e98f1c2e
0xMikey-ooze <119510170+0xMikey-ooze@users.noreply.github.com>
2026-03-18 18:32:29 -0400

    fix: add standalone output for Docker runner

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {}
-
+import type { NextConfig } from "next"
+const nextConfig: NextConfig = { output: "standalone" }
 export default nextConfig
+
